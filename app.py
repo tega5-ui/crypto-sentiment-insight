c54a58221a111b94714ed4e1045fe6bb0ee78589
tega5-ui <benshkban@gmail.com>
2025-06-21 04:17:16 +0200

    app.py
    Update

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1iuSRSUCDuwM2UUd2lFhOPlGKmDUUrTEG
 """
 
-!pip install textblob
 from textblob import TextBlob
 
 !pip install yfinance
